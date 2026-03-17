5b94f55ce488fcbc9768d2b6fcd0c203c508550d
Aaron Qiu <aaron.qiu345@gmail.com>
2026-03-16 18:16:18 -0700

    fixed main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import graphReader
-import breadthFinder
-import depthFinder
+from graphReader import read
+from breadthFinder import breadth
+from depthFinder import depth
 
 while True:
   todo = input("DFS, BFS, or READ? ")
   if todo.upper() == "DFS":
     print("Depth-First-Search selected.")
     start = input("Where do you want to start from? ")
-    target = input("Where do you want to end at?" )
+    target = input("Where do you want to end at? " )
     depth(start, target)
     break
   if todo.upper() == "BFS":
